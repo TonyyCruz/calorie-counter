fb8cfcaa7798edcc9dc3055ca1085e00d2f28731
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-03-19 14:06:57 -0300

    fix: user delete controller had a error, was missing a build

--- src/main/java/com/anthony/calorie_counter/controller/UserController.java
+++ src/main/java/com/anthony/calorie_counter/controller/UserController.java
@@ -5,6 +5,7 @@ import com.anthony.calorie_counter.dto.response.UserView;
 import com.anthony.calorie_counter.entity.User;
 import com.anthony.calorie_counter.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,8 +36,9 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    ResponseEntity.HeadersBuilder<?> delete(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    ResponseEntity<?> delete(@PathVariable Long id) {
         userService.delete(id);
-        return ResponseEntity.noContent();
+        return ResponseEntity.noContent().build();
     }
 }
