c9be68d2b20632132ba9fdd1491922ed495ee547
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-04-02 18:13:15 -0300

    feat: add validation to user entity and dto

--- src/main/java/com/anthony/calorie_counter/dto/response/UserView.java
+++ src/main/java/com/anthony/calorie_counter/dto/response/UserView.java
@@ -4,11 +4,6 @@ import com.anthony.calorie_counter.entity.User;
 
 public record UserView(String name, String email) {
 
-    public UserView(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
     public UserView(User user) {
         this(user.getName(), user.getEmail());
     }
