820f518bcbe6c487d904247b0477c62efa447924
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-04-10 13:11:00 -0300

    test: create sucess caser to user controller

--- src/test/java/com/anthony/calorie_counter/integration/UserControllerIntegrationTest.java
+++ src/test/java/com/anthony/calorie_counter/integration/UserControllerIntegrationTest.java
@@ -10,8 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,12 +20,12 @@ public class UserControllerIntegrationTest extends TestBase {
 
     @Test @DisplayName("Test if is possible create a new user.")
     void canCreateAnNewUser() throws Exception {
-        UserDto userDto = UserFactory.createUserDto();
-        String valueAsString = objectMapper.writeValueAsString(userDto);
+        UserDto newUser = UserFactory.createUserDto();
+        String valueAsString = objectMapper.writeValueAsString(newUser);
         mockMvc.perform(post(USER_URL).contentType(MediaType.APPLICATION_JSON).content(valueAsString))
             .andExpect(status().isCreated())
-            .andExpect(jsonPath("$.fullName").value(userDto.fullName()))
-            .andExpect(jsonPath("$.email").value(userDto.email()))
+            .andExpect(jsonPath("$.fullName").value(newUser.fullName()))
+            .andExpect(jsonPath("$.email").value(newUser.email()))
             .andExpect(jsonPath("$.password").doesNotExist());
     }
 
@@ -38,6 +37,30 @@ public class UserControllerIntegrationTest extends TestBase {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.fullName").value(user.getFullName()))
                 .andExpect(jsonPath("$.email").value(user.getEmail()))
+                .andExpect(jsonPath("$.password").doesNotExist());
+    }
+
+    @Test @DisplayName("Test if is possible update a user by received id.")
+    void canUpdateAnUserById() throws Exception {
+        User currentUser = UserFactory.createUser();
+        UserDto updateUser = UserFactory.createUserDto();
+        String valueAsString = objectMapper.writeValueAsString(updateUser);
+        userRepository.save(currentUser);
+        mockMvc.perform(put(USER_URL + "/1").contentType(MediaType.APPLICATION_JSON).content(valueAsString))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.fullName").value(updateUser.fullName()))
+                .andExpect(jsonPath("$.email").value(updateUser.email()))
+                .andExpect(jsonPath("$.password").doesNotExist());
+    }
+
+    @Test @DisplayName("Test if is possible update a user by received id.")
+    void canDeleteAnUserById() throws Exception {
+        User user = UserFactory.createUser();
+        userRepository.save(user);
+        mockMvc.perform(delete(USER_URL + "/1"))
+                .andExpect(status().isNoContent())
+                .andExpect(jsonPath("$.fullName").doesNotExist())
+                .andExpect(jsonPath("$.email").doesNotExist())
                 .andExpect(jsonPath("$.password").doesNotExist());
     }
 }
