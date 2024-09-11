3615b098430415c5683461c248559a60a097fdff
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-09-11 16:36:33 -0300

    build: add hibernate search

---
+++ src/main/java/com/anthony/calorie_counter/service/validation/AlimentStringFieldValid.java
@@ -0,0 +1,21 @@
+package com.anthony.calorie_counter.service.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface PasswordValid {
+    String message() default "Validation error.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
