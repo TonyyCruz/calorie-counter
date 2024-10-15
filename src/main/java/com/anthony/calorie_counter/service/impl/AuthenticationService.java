b9ade80e092bfc7ed7b61ef9761fe54764a41b95
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-10-15 16:41:22 -0300

    refactor: AuthenticationService

--- src/main/java/com/anthony/calorie_counter/service/impl/AuthenticationService.java
+++ src/main/java/com/anthony/calorie_counter/service/impl/AuthenticationService.java
@@ -2,17 +2,15 @@ package com.anthony.calorie_counter.service.impl;
 
 import com.anthony.calorie_counter.config.security.JwtService;
 import com.anthony.calorie_counter.service.interfaces.IAuthenticationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService implements IAuthenticationService {
     private final JwtService jwtService;
-
-    public AuthenticationService(JwtService jwtService) {
-        this.jwtService = jwtService;
-    }
-
+    
     @Override
     public String authenticate(Authentication  authentication) {
         return jwtService.generateToken(authentication);
