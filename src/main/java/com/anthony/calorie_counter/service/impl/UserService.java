1f878b272c67d35d2ffc6d769e26530bae94e1e8
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-10-15 16:40:09 -0300

    refactor: UserService remove transaction annotation. replace local constructor for lombook annotation

--- src/main/java/com/anthony/calorie_counter/service/impl/UserService.java
+++ src/main/java/com/anthony/calorie_counter/service/impl/UserService.java
@@ -8,6 +8,7 @@ import com.anthony.calorie_counter.exceptions.messages.ExceptionMessages;
 import com.anthony.calorie_counter.repository.UserRepository;
 import com.anthony.calorie_counter.service.interfaces.IUserService;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,24 +20,18 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class UserService implements IUserService, UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
     @Override
-    @Transactional(readOnly = true)
     public UserModel findById(UUID id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new EntityDataNotFoundException(ExceptionMessages.USER_NOT_FOUND_WITH_ID + id));
     }
 
     @Override
-    @Transactional
     public UserModel create(UserModel userModel) {
         userModel.addRole(new RoleModel(UserRole.ROLE_USER));
         userModel.setPassword(passwordEncoder.encode(userModel.getPassword()));
@@ -44,7 +39,6 @@ public class UserService implements IUserService, UserDetailsService {
     }
 
     @Override
-    @Transactional
     public UserModel updateUser(UUID id, UserModel newUserModelData) {
         try {
             UserModel userModel = userRepository.getReferenceById(id);
@@ -58,7 +52,6 @@ public class UserService implements IUserService, UserDetailsService {
     }
 
     @Override
-    @Transactional
     public void updatePassword(UUID id, String newPassword) {
         try {
             UserModel userModel = userRepository.getReferenceById(id);
@@ -70,7 +63,6 @@ public class UserService implements IUserService, UserDetailsService {
     }
 
     @Override
-    @Transactional
     public void deleteById(UUID id) {
         if (!userRepository.existsById(id)) {
             throw new EntityDataNotFoundException(ExceptionMessages.USER_NOT_FOUND_WITH_ID + id);
@@ -79,13 +71,11 @@ public class UserService implements IUserService, UserDetailsService {
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Page<UserModel> findAll(Pageable pageable) {
         return userRepository.findAll(pageable);
     }
 
     @Override
-    @Transactional
     public UserModel promoteToAdmin(UUID id) {
         try {
             UserModel userModel = userRepository.getReferenceById(id);
@@ -97,7 +87,6 @@ public class UserService implements IUserService, UserDetailsService {
     }
 
     @Override
-    @Transactional
     public UserModel demoteFromAdmin(UUID id) {
         try {
             UserModel userModel = userRepository.getReferenceById(id);
