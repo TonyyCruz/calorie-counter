90b3eb1c6920dc4b4fc5e3968a95c0bdd1ccc16c
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-10-28 05:46:38 -0300

    refactor: refactor interfaces to use the dto instead of the model

--- src/main/java/com/anthony/calorie_counter/entity/dto/request/MealDto.java
+++ src/main/java/com/anthony/calorie_counter/entity/dto/request/MealDto.java
@@ -1,4 +1,17 @@
 package com.anthony.calorie_counter.entity.dto.request;
 
-public class MealDto {
+import com.anthony.calorie_counter.service.validation.MealValid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@MealValid
+public class MealDto implements Serializable {
+    private Long descriptionId;
+    private Long dailyConsumeId;
 }
--- src/main/java/com/anthony/calorie_counter/service/interfaces/IMealService.java
+++ src/main/java/com/anthony/calorie_counter/service/interfaces/IMealService.java
@@ -1,18 +1,19 @@
 package com.anthony.calorie_counter.service.interfaces;
 
 import com.anthony.calorie_counter.entity.MealModel;
+import com.anthony.calorie_counter.entity.dto.request.MealDto;
 
 import java.util.List;
 
 public interface IMealService {
 
-    MealModel create(MealModel meal);
+    MealModel create(MealDto mealDto);
 
     MealModel findById(Long id);
 
     List<MealModel> findAllByDailyConsumeId(Long dailyConsumeId);
 
-    MealModel updateById(Long id, MealModel meal);
+    MealModel update(Long id, MealDto mealDto);
 
     void deleteById(Long id);
 }
