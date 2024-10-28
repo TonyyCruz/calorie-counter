4fef6fe2e918586e84f6935385d61dd11c10c47f
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-10-28 07:35:15 -0300

    feat-meal: add route to getby id, ge all by daily consumption and delete by id

--- src/main/java/com/anthony/calorie_counter/controller/MealController.java
+++ src/main/java/com/anthony/calorie_counter/controller/MealController.java
@@ -5,14 +5,14 @@ import com.anthony.calorie_counter.entity.dto.request.MealDto;
 import com.anthony.calorie_counter.entity.dto.response.MealViewDto;
 import com.anthony.calorie_counter.service.interfaces.IMealService;
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,5 +24,31 @@ public class MealController {
     public ResponseEntity<MealViewDto> create(@RequestBody @Valid MealDto mealDto) {
         MealModel meal = mealService.create(mealDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(new MealViewDto(meal));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MealViewDto> findById(@PathVariable Long id) {
+        MealModel meal = mealService.findById(id);
+        return ResponseEntity.ok(new MealViewDto(meal));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MealViewDto>> findAllByDailyConsumeId(@PathParam("dailyConsumeId") Long dailyConsumeId) {
+        List<MealViewDto> mealList = mealService.findAllByDailyConsumeId(dailyConsumeId)
+                .stream().map(MealViewDto::new).toList();
+        return ResponseEntity.ok(mealList);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MealViewDto> update(@RequestBody @Valid MealDto mealDto, @PathVariable Long id) {
+        MealModel meal = mealService.update(id, mealDto);
+        return ResponseEntity.ok(new MealViewDto(meal));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        mealService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
