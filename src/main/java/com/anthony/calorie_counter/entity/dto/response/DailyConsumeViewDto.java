1dd1c212894649db1c11707635ed5ae776602c08
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-10-30 06:19:37 -0300

    feat: add meal list to DailyConsumeVieDto

--- src/main/java/com/anthony/calorie_counter/entity/dto/response/DailyConsumeViewDto.java
+++ src/main/java/com/anthony/calorie_counter/entity/dto/response/DailyConsumeViewDto.java
@@ -3,10 +3,16 @@ package com.anthony.calorie_counter.entity.dto.response;
 import com.anthony.calorie_counter.entity.DailyConsumeModel;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
-public record DailyConsumeViewDto(Long Id, UUID userId, Instant date) {
+public record DailyConsumeViewDto(Long Id, UUID userId, Instant date, List<MealViewDto> meals) {
     public DailyConsumeViewDto(DailyConsumeModel dailyConsume) {
-        this(dailyConsume.getId(), dailyConsume.getUser().getId(), dailyConsume.getDate());
+        this(
+                dailyConsume.getId(),
+                dailyConsume.getUser().getId(),
+                dailyConsume.getDate(),
+                dailyConsume.getMeals().stream().map(MealViewDto::new).toList()
+        );
     }
 }
