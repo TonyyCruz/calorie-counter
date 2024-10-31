451b694754871289c560ddd7665e993c8c011439
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-10-31 07:50:23 -0300

    feat: add routes to find DailyConsumes by user id and. add route to find DailyConsume by user id and date.

--- src/main/java/com/anthony/calorie_counter/service/interfaces/IDailyConsumeService.java
+++ src/main/java/com/anthony/calorie_counter/service/interfaces/IDailyConsumeService.java
@@ -1,10 +1,11 @@
 package com.anthony.calorie_counter.service.interfaces;
 
-
 import com.anthony.calorie_counter.entity.DailyConsumeModel;
 import com.anthony.calorie_counter.entity.dto.request.DailyConsumeDto;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IDailyConsumeService {
@@ -15,7 +16,9 @@ public interface IDailyConsumeService {
 
     List<DailyConsumeModel> findAllByUserId(UUID userId);
 
-    DailyConsumeModel update(Long id, DailyConsumeModel dailyConsume);
+    DailyConsumeModel findByUserIdAndDate(UUID userId, Instant date);
+
+//    DailyConsumeModel update(Long id, DailyConsumeModel dailyConsume);
 
     void delete(Long id);
 }
